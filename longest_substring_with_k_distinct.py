d6fe29579b63e9ff8d81eec59bc6213e163c1f1b
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-05-31 23:42:29 -0400

    Update longest_substring_with_k_distinct.py

--- longest_substring_with_k_distinct.py
+++ longest_substring_with_k_distinct.py
@@ -1,25 +1,51 @@
-def longest_substring_with_k_distinct(str, k):
+def longest_substring_with_k_distinct(str_given, k):
   """ sliding window (since substrings are not reorderable)
   keep track of:
   the unique character count
     hash table
       char: count
-  length of the substring
+  max length of the substring
     return value
     we are trying to maximize this
   window_start = 0
   window_end
+  window size
   
   logic:
   loop through length of string with the window_end
     append character to hash table
-      if character is already in hash table, count += 1
-      else count = 1
+      if character is not already in hash table, count = 0
+    count += 1
     
-    loop through all keys of the hash table
-      if the unique character (value of hash table) count is greater than k
+    while the unique character (value of hash table) count is greater than k
       shrink the current window
+        subtract 1 from the frequency of the first character in substring since that is part of shrinking
         window_start += 1
+    
+    check if there is a new max, save if true
   
   """
+  window_start = 0
+  max_length = 0
+  char_counts = {}
+
+  for window_end in range( len(str_given) ):
+    end_char = str_given[window_end] 
+    if end_char not in char_counts:
+      char_counts[end_char] = 0
+    char_counts[end_char] += 1
+
+    while len(char_counts) > k:
+      start_char = str_given[window_start]
+      char_counts[start_char] -= 1
+      # when the count hits zero delete the character b/c it contributes to length 
+      if char_counts[start_char] == 0:
+        del char_counts[start_char]
+      window_start += 1
+
+    if window_end - window_start + 1 > max_length:
+      max_length = window_end - window_start + 1
+
+  return max_length
+
   return -1
