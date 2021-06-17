68753efbfe662079b1532941c25b8d4c07357f86
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-06-16 21:13:08 -0400

    Update linkedlist__length_of_cycle.py

--- linkedlist__length_of_cycle.py
+++ linkedlist__length_of_cycle.py
@@ -24,5 +24,5 @@ def length_cycle(tortoise):
     if curr == tortoise:
       return length
 
-# O(N) time complexity, where N is the length of given linked list.
-# O(1) space complexity.
+# O(N) time complexity, where N is the length of given linked list, since in the first function, the hare meets the tortoise in the same loop once the tortoise enters the cycle. Then we allow the a copy of the tortoise to travel through the cycle once more to count the length. In the worst case, the cycle is the length of the linkedlist. This is traversing twice in the worst case as well giving us 2N which is still asymptotically N.
+# O(1) space complexity since we are not creating new data structures.
