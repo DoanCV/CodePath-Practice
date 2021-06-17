1dff8b6fd32edc745a5a0316bcaa2ba669c580f8
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-06-16 20:52:06 -0400

    Create linkedlist_cycle.py

---
+++ linkedlist_cycle.py
@@ -0,0 +1,18 @@
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+def has_cycle(head):
+  hare = head
+  tortoise = head
+  while hare is not None and tortoise is not None:
+    hare = hare.next.next
+    tortoise = tortoise.next
+    if hare == tortoise:
+      return True
+  return False
+
+# O(N) time complexity, where N is the length of the array, since the hare will meet the tortoise in the same loop.
+# O(1) space complexity, since we are not creating any new data structures.
