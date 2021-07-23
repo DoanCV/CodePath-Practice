4d1f6bdfee323978fb98c144b1f05116182bbca9
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-07-23 00:41:11 -0400

    Update all_paths_for_a_sum.py

--- Python/all_paths_for_a_sum.py
+++ Python/all_paths_for_a_sum.py
@@ -14,12 +14,20 @@ def recurse(currentNode, sum, currentPath, allPaths):
   if currentNode is None:
     return 
   
+  # add the current node to the path
   currentPath.append(currentNode.val)
-
+  
+  # Check for leaf node and if the value of the node 
   if currentNode.val == sum and currentNode.left is None and currentNode.right is None:
     allPaths.append(list(currentPath))
   else:
+    # traverse the left subtree
     recurse(currentNode.left, sum - currentNode.val, currentPath, allPaths)
+    # traverse the right subtree
     recurse(currentNode.right, sum - currentNode.val, currentPath, allPaths)
-
+  
+  # backtrack 
   currentPath.pop()
+
+# O(N) time complexity, where N is the number of nodes in the given binary tree, since we visit each node once.
+# O(N) space complexity since in the worst case each node has only one child like a linkedlist.
