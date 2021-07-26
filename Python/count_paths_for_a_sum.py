4883ace5ea5c4841207b14a20f1368cef9032d9b
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-07-26 00:28:25 -0400

    Update count_paths_for_a_sum.py

--- Python/count_paths_for_a_sum.py
+++ Python/count_paths_for_a_sum.py
@@ -1,3 +1,8 @@
+"""
+Given a binary tree and a number ‘S’, find all paths in the tree such that the sum of all the node values of each path equals ‘S’. 
+Please note that the paths can start or end at any node but all paths must follow direction from parent to child (top to bottom).
+"""
+
 class TreeNode:
   def __init__(self, val, left=None, right=None):
     self.val = val
@@ -7,6 +12,8 @@ class TreeNode:
 
 def count_paths(root, S):
   """
+  DFS recursion
+  We have to keep track of the path we are on so we can check if there is a full or partial path length that is valid
   """
   return dfs(root, S, [])
 
@@ -29,6 +36,10 @@ def dfs(curr_node, S, curr_path):
   valid_paths += dfs(curr_node.left, S, curr_path)
   valid_paths += dfs(curr_node.right, S, curr_path)
 
+  # backtrack to process other paths in the recursive call stack
   curr_path.pop()
 
   return valid_paths
+
+# O(N) time complexity, where N is the number of nodes in the given binary tree, since we visit each node once.
+# O(N) space complexity. We are using a curr_path array which in the worst case will hold the entire tree since each node would have only one child like a linkedlist. There is also our recursive call stack which would be the same size.
