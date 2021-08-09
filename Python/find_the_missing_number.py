68b470b028233225b54dc639ea7f1864eb0964c7
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-08-09 19:18:40 -0400

    Update find_the_missing_number.py

--- Python/find_the_missing_number.py
+++ Python/find_the_missing_number.py
@@ -15,3 +15,21 @@ def find_missing_number(nums):
     curr_sum += nums[i]
 
   return len(nums) * (len(nums) + 1) / 2 - curr_sum 
+
+
+def find_missing_number(nums):
+
+  i = 0
+  while i < len(nums):
+    correct = nums[i]
+    if correct < len(nums) and nums[i] != nums[correct]:
+      nums[i], nums[correct] = nums[correct], nums[i]
+    else:
+      i += 1
+  
+  for i in range(len(nums)):
+    if i != nums[i]:
+      return i
+    
+# O(N) time complexity, where N is the length of the given array, since in the worst case we will swap n - 1 times before incrementing i and then we find that out missing number is at the end.
+# O(1) space complexity since we are not using any extra data structures.
