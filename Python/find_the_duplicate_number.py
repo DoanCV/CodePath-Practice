5a2a2a0e35f8ddfe44783c5d7d6828333d4e7eff
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-08-10 18:24:41 -0400

    Update find_the_duplicate_number.py

--- Python/find_the_duplicate_number.py
+++ Python/find_the_duplicate_number.py
@@ -1,11 +1,32 @@
 """
 # we only have one number being duplicated but it can be duplicated multiple times
+# we are guranteed a duplicate 
 
 Cyclic sort:
+while we are not at the end of the array
+
+  if the current value is in the wrong position
+
+    if the value at the position of the current value is not the same 
+      we swap
+    otherwise the two values we are swapping are the same then we found our duplicate
+
+  else
+    increment the current position
 
 """
 
 def find_duplicate(nums):
-  
-  
-  return 
+  i = 0
+  while i < len(nums):
+    
+    if nums[i] - 1 != i:
+
+      current = nums[i] - 1
+      if nums[i] != nums[current]:
+        nums[i], nums[current] = nums[current], nums[i]  # swap
+      else:  # we have found the duplicate
+        return nums[i]
+
+    else:
+      i += 1
