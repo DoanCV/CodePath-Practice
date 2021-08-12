69269217efd4e851fb179f11a54d967fd5c97909
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-08-12 00:05:29 -0400

    Update find_the_duplicate_number.py

--- Python/find_the_duplicate_number.py
+++ Python/find_the_duplicate_number.py
@@ -60,3 +60,59 @@ def find_duplicate(nums):
   
 # O(N) time complexity, where N is the length of the given array, since in the worst case we complete everything in one loop with no repeated steps.
 # O(1) space complexity since we are not using any extra data structures.
+
+"""
+Follow up:
+Can you do this without modifiying the input array and still use O(1) space?
+Joma Tech (Anime): Trivial
+
+We can use tortoise and hare to find a cycle and then we find the start of this cycle. 
+We have a cycle because of the Pigeonhole principle and the assumption that we have N + 1 numbers from 1 to N.
+The start of the cycle is the duplicate number.
+
+# find the cycle
+
+# find the length of the cycle
+
+# jump the fast pointer (now slow) back to the start of the array
+
+# move both these pointers until they meet, this is the start of the cycle
+
+ex. nums = [1,4,4,3,2]
+slow = nums[0] = 1
+"""
+
+def find_duplicate(nums):
+
+  # find cycle
+  slow = nums[0]
+  fast = nums[nums[0]]
+  while slow != fast:
+    slow = nums[slow]
+    fast = nums[nums[fast]]
+  
+  # find length of cycle
+    # create another pointer go to the next element after where slow left off in the cycle and keep going until we find the slow pointer again
+  curr = nums[nums[slow]]
+  cycle_length = 1
+  while start != nums[slow]:
+    curr = nums[curr]
+    cycle_length += 1
+  
+  return cycle_start(nums, cycle_length)
+
+def cycle_start(nums, cycle_length):
+  # move one pointer to cycle_length
+  ptr1 = nums[0]
+  ptr2 = nums[0]
+  while cycle_length > 0:
+    ptr2 = nums[ptr2]
+    cycle_length -= 1
+
+  # move both pointers one at a time until they meet
+  while ptr1 != ptr2:
+    ptr1 = nums[ptr1]
+    ptr2 = nums[ptr2]
+
+  # return the value that they are the same, this is the start of the cycle
+  return ptr1
