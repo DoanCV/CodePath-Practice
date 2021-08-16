af11d2f25f816e51da697c6b0334f0bcaec80f8f
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-08-16 12:33:15 -0400

    Create top_k_numbers.py

---
+++ Python/top_k_numbers.py
@@ -0,0 +1,30 @@
+from heapq import *
+
+"""
+Use a min heap
+  we need the largest numbers and in heapq when we pop we get the smallest element since they get higher priority
+  so we will insert only k elements into the heap
+  if the current value is greater than what is in the heap, we remove the smallest element and push the current value
+"""
+
+def find_k_largest_numbers(nums, k):
+  heap = []
+
+  # add the first k elements into the heap
+  for i in range(k):
+    heappush(heap, nums[i])
+
+  # go through the rest of the array and check if the top of the heap is less than the current element
+    # if so pop and then push the new element into the heap
+  for i in range(k, len(nums)):
+    if heap[0] < nums[i]:
+      heappop(heap)
+      heappush(heap, nums[i])
+
+  # return the heap as a list, which is size k and has the k highest elements
+
+  return list(heap)
+  
+  
+ # O(N) time complexity, where N is the length of the given array, since we traverse through the array once and do not repeat any calculations.
+ # O(k) space complexity since we keep out heap to size k.
