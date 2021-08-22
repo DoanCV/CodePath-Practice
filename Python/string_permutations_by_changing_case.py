cc6de43628b158e0d8374b0162db40251c96000e
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-08-22 18:36:19 -0400

    Update string_permutations_by_changing_case.py

--- Python/string_permutations_by_changing_case.py
+++ Python/string_permutations_by_changing_case.py
@@ -11,11 +11,18 @@ def find_letter_case_string_permutations(str):
   # loop through each character
   for i in range(len(str)):
     # if curr char is a letter
+    if str[i].isalpha():
       # go through all the previous permutations
+      n = len(permutations)
+      for j in range(n):
         # make a copy of each and change the capitalization of the current letter in the copy
+          # string is immutable so we need to make the change in a list
+        copy = list(permutations[j])
+        copy[i] = copy[i].swapcase()        
         # append it to permutations
-  
+        permutations.append("".join(copy))
   return permutations
+
 
 
 def main():
@@ -26,3 +33,6 @@ def main():
 
 
 main()
+
+# O(N * 2^N) time complexity, where N is the length of the given string, since for each character there are two permutations and in the worst case the entire string is all alphabeticals.
+# O(N * 2^N) space complexity since that is the size of the output.
