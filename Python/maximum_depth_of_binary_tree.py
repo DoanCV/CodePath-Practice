db7ae68bb861b278e3e5c15c8e0583cdd551fe9d
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-08-29 17:43:38 -0400

    Update maximum_depth_of_binary_tree.py

--- Python/maximum_depth_of_binary_tree.py
+++ Python/maximum_depth_of_binary_tree.py
@@ -35,10 +35,45 @@ def find_maximum_depth(root):
 # O(N) time complexity, where N is the number of nodes in the given binary tree, since we have to visit every node once.
 # O(N) space complexity since in the worst case the bottom level will have N/2 nodes which is asymptotically N.
 
-
-
 """
-DFS solution
+DFS iterative solution
+We use a stack and store (node, depth)
+"""
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        """
+        Iterative DFS, using a stack
+        """
+        maxDepth = 0
+        if root is None:
+            return maxDepth
+        
+        stack = []
+        stack.append((root, 1))
+        
+        while stack:
+            
+            # pop from stack to get current node and depth
+            curr_node, curr_depth = stack.pop()
+            
+            if curr_node is None:
+                continue
+            
+            # check if leaf node
+            # if it has children, push into stack with depth + 1
+            if curr_node.left or curr_node.right:
+                stack.append((curr_node.left, curr_depth + 1))
+                stack.append((curr_node.right, curr_depth + 1))
+        
+            # check if depth is greater than maxDepth, if so update
+            if curr_depth > maxDepth:
+                maxDepth = curr_depth
+        
+        # return maxDepth
+        return maxDepth
+      
+"""
+DFS recursive solution
 Compare left and right subtree
 """
 
