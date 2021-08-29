0494c33685fb89f77ce66a20cddc811d727e5b7e
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-08-29 13:11:08 -0400

    Update best_time_to_buy_and_sell_stock.py

--- Python/best_time_to_buy_and_sell_stock.py
+++ Python/best_time_to_buy_and_sell_stock.py
@@ -1,3 +1,50 @@
+"""
+ex.
+prices = [7,1,5,3,6,4]
+
+
+window_end = 0
+max = 0
+window_start = 0
+    bc 7-7 = 0 
+
+
+window_end = 1
+max = 0
+    bc 1 - 7 < 0
+window_start = 1
+    bc prices[window_start] > prices[window_end]
+
+
+window_end = 2
+max = 4
+    bc 5 - 1 = 3 and is > 0
+window_start = 1
+
+
+window_end = 3
+max = 4
+    3 - 1 = 2 < 4
+window_start = 1
+
+
+window_end = 4
+max = 5
+    6 - 1 = 5 and is > 4
+window_start = 1
+
+
+window_end = 5
+max = 5
+    4 - 1 = 3 and is < 5
+window_start = 1
+
+
+The idea is that we are trying to get window_start to the lowest value possible and then window_end to the highest value possible
+    all in one pass
+
+"""
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         """
