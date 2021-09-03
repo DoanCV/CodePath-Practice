070563925934c418578eebd41331ee1c436d9f54
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-09-02 23:57:57 -0400

    Update lru_cache.py

--- Python/lru_cache.py
+++ Python/lru_cache.py
@@ -1,1 +1,28 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
 
+class LRUCache:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def get(self, key: int) -> int:
+        
+
+    def put(self, key: int, value: int) -> None:
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
