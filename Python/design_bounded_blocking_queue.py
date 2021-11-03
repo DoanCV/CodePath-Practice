843cb45c93e106f26ecbf2a6bb6c689f2a73fdcb
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-11-02 22:47:36 -0400

    Create design_bounded_blocking_queue.py

---
+++ Python/design_bounded_blocking_queue.py
@@ -0,0 +1,24 @@
+"""
+Implement a thread safe bounded blocking queue that has the following methods:
+  - Constructor intializes the queue with a maximum capacity
+  - Enqueue() adds and element to the front of the queue. If the queue is full, the calling thread is blocked until the queue is no longer full
+  - Dequeue() returns the element at the rear of the queue and removes it. If the queue is empty, the calling thread is blocked until the queue is no longer empty
+  - Size() returns the number of elements currently in the queue 
+  
+
+
+"""
+
+class BoundedBlockingQueue(object):
+  
+  def __init__(self, capacity: int):
+    
+    
+  def enqueue(self, element: int) -> None:
+    
+    
+  def dequeue(self) -> int:
+  
+  
+  def size(self) -> int:
+    
