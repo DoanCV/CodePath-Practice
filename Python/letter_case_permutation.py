ea379a17cf5189a886f5767e04e8c0bf73834e89
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-11-14 13:40:12 -0500

    Update letter_case_permutation.py

--- Python/letter_case_permutation.py
+++ Python/letter_case_permutation.py
@@ -7,7 +7,7 @@ Iterative version
 "a1b2c3" "A1b2c3"                                                           <= i = 1, ignore '1', which is a digit,
 "a1b2c3" "A1b2c3" {"a1B2c3" "A1B2c3"}                                       <= i = 2, b toggled to B
 "a1b2c3" "A1b2c3" "a1B2c3" "A1B2c3"                                         <= i = 3, ignore '2', which is a digit,
-"a1b2c3" "A1b2c3" "a1B2c3" "A1B2c3" {"a1b2C3" "A1b2C3" "a1B2C3" "A1B2C3"}    <= i = 4, c toggled to C
+"a1b2c3" "A1b2c3" "a1B2c3" "A1B2c3" {"a1b2C3" "A1b2C3" "a1B2C3" "A1B2C3"}   <= i = 4, c toggled to C
 "a1b2c3" "A1b2c3" "a1B2c3" "A1B2c3" "a1b2C3" "A1b2C3" "a1B2C3" "A1B2C3"     <= i = 5, ignore 3, which is a digit.
 
 O(n * 2^n) time complexity where n is the length of the string. There are 2^n combinations since there are only 2 cases, upper and lower, and in the worst case our entire string is all letters. When we generate permutations we go through all of our previous permutations which are length n.
