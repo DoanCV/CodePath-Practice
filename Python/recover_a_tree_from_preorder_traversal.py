6b565757ad24e612bcac7eb5734dd067bf4208c0
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-11-16 00:54:18 -0500

    Update recover_a_tree_from_preorder_traversal.py

--- Python/recover_a_tree_from_preorder_traversal.py
+++ Python/recover_a_tree_from_preorder_traversal.py
@@ -23,16 +23,62 @@ With this string recover the tree, ie. build it, and return the root of the tree
     since we use dashes we have to constrain the node values to positives since otherwise the "-" could mean negative value
     also keep in mind that the input is a string so we have to scan for entire node values
 
+Nodes with one child means that the child is a left child
 
-DFS recursion with helper function
 
-First we split the input into levels based on the number of dashes
-After we split we have a few possibilites
-    No splitters, this means our entire string is a node value
-    One splitter, ie one sided tree
-    Two splitters, ie we have left and right subtree
+
+
+DFS iterative
+
+Use a hashmap to store the level and the nodes at each level
+
+First we scan the input for two cases:
+    levels based on the number of dashes or the digits
+    note that apart from the root of the tree, all nodes are preceeded by dashes
     
+Once we finish scanning a digit we know we have a node so we can add to the tree
+
+potential logic challenge:
+    each level may have many nodes
+    we will reset it each time we find the same level again since by the time we find the next level the current one would already have been completely built out
+
+
+O(N) time complexity where N is the length of the given string since we solve in one pass. We iterate through each character of the string and we revisit parent nodes by taking advantage of constant time lookups with our hashmap.
+O(numNodes) space complexity since we use a hashmap that relates the depth to the node at that depth that we will use to build the tree. The numNodes is the worst case depth of the binary tree and that will add keys to our hashmap.
 """
+from collections import defaultdict
 class Solution:
     def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
-      
+        
+        hashmap = defaultdict()
+        
+        i = 0
+        while i < len(traversal):
+            curr_level = 0
+            curr_node_val = 0
+            
+            # we found dashes
+            while i < len(traversal) and traversal[i] == "-":
+                curr_level += 1
+                i += 1
+            
+            # we found digits
+                # do not change to int in the while statement, only do that inside the loop, since "-" is not in between "0" - "9" and we can use this fact
+            while i < len(traversal) and traversal[i] >= "0" and traversal[i] <= "9": 
+                curr_node_val = curr_node_val * 10 + int(traversal[i])
+                i += 1
+            
+            # create the node, add the current node to the corresponding level in the hashamp and add to tree using the parent node
+            curr_node = TreeNode(curr_node_val)
+            hashmap[curr_level] = curr_node
+            
+            if curr_level > 0:
+                parent_node = hashmap[curr_level - 1]
+                
+                # default current node to child of parent otherwise make that the right child
+                if parent_node.left is None:
+                    parent_node.left = curr_node
+                else:
+                    parent_node.right = curr_node 
+        
+        return hashmap[0]
