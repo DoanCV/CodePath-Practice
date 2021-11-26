a2fbfd02196f160a0a0ed142e9113e8ebaa8f2fa
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-11-26 18:48:32 -0500

    Create zuma_game.py

---
+++ Python/zuma_game.py
@@ -0,0 +1,4 @@
+# Brute force is probably the best way, most greedy methods miss the test case that simulates this insane clip: https://www.youtube.com/watch?v=kBbGptm9v7A
+
+class Solution:
+    def findMinStep(self, board: str, hand: str) -> int:
