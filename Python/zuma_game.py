756cdca8f6998ffcd041f7d13cef612147401b1b
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-11-26 19:31:58 -0500

    Update zuma_game.py

--- Python/zuma_game.py
+++ Python/zuma_game.py
@@ -2,3 +2,31 @@
 
 class Solution:
     def findMinStep(self, board: str, hand: str) -> int:
+        @lru_cache(None)
+        def clean(baord):
+            # sliding window to crush like in 1D candy crush
+        
+        @lru_cache(None)
+        def dfs(board, hand):
+            if not board:
+                return 0
+            if not hand:
+                return float('inf')
+            
+            
+            m = len(board)
+            ans = float('inf')
+            for j, b in enumerate(hand):
+                new_hand = hand[:j] + hand[j+1:]
+                for i in range(m + 1):
+                    new_board = clean(board[:i] + b + board[i:])
+                    ans = min(ans, 1 + dfs(new_board, new_hand))
+            return ans
+        
+        
+        ans = dfs(board, hand)
+        
+        if ans < float('inf'):
+            return ans
+        else:
+            return -1
