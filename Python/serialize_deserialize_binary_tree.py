df13ee7e878fd104a72770b0b65d0c1b22eeadeb
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-12-22 01:25:26 -0500

    Update serialize_deserialize_binary_tree.py

--- Python/serialize_deserialize_binary_tree.py
+++ Python/serialize_deserialize_binary_tree.py
@@ -27,6 +27,12 @@ O(N) time since we visit each node once
 Deserialize
 use a queue to reconstruct the tree
     discard the "x" when we see it
+    
+queue = [1,2,x,x,3,4,x,x,5,x,x]
+
+1                           root
+(2, x, x)                   left
+(3, (4, x, x), (5, x, x))   right
 
 O(N) time since we visit each element of data once
 
