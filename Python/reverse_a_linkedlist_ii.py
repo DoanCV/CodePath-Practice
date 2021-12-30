e38283c4d9060d60fa6d69c56c5f4d8a1712af22
DoanCV <72458331+DoanCV@users.noreply.github.com>
2021-12-29 21:21:12 -0500

    Update reverse_a_linkedlist_ii.py

--- Python/reverse_a_linkedlist_ii.py
+++ Python/reverse_a_linkedlist_ii.py
@@ -8,9 +8,11 @@ class Solution:
         if not head or left == right:
             return head
         
+        # dummy head to easily return the head of new linkedlist
         dummy = ListNode(None)
         dummy.next = head
         
+        # go up to the node right before the left of the range, keep in mind nodes are indexed from 1 as given in the examples
         start = dummy
         for _ in range(left - 1):
             start = start.next
@@ -25,7 +27,12 @@ class Solution:
             prev = curr
             curr = temp
         
+        # 1 -> 2 <- 3 <- 4 5
+            # curr = 5
+            # start = 1
+            # prev = 4
         start.next.next = curr
         start.next = prev
+        # 1 -> 4 -> 3 -> 2 -> 5
         
         return dummy.next
