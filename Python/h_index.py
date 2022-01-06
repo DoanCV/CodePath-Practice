80f3d17a564fa522d7e699091578988cf55e8f87
DoanCV <72458331+DoanCV@users.noreply.github.com>
2022-01-05 21:15:21 -0500

    Update h_index.py

--- Python/h_index.py
+++ Python/h_index.py
@@ -1,4 +1,6 @@
 """
+sort in descending order then find the last position where the value is greater than the index
+
 From the Wikipedia article: 
 "Formally, if f is the function that corresponds to the number of citations for each publication, we compute the h-index as follows: First we order the values of f from the largest to the lowest value. Then, we look for the last position in which f is greater than or equal to the position (we call h this position)"
 """
