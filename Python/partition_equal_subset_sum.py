b8112ac6863313438a5bca06ba5ba1eaf2fe6aec
DoanCV <72458331+DoanCV@users.noreply.github.com>
2022-01-09 00:40:24 -0500

    Update partition_equal_subset_sum.py

--- Python/partition_equal_subset_sum.py
+++ Python/partition_equal_subset_sum.py
@@ -1,7 +1,24 @@
 """
+U
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+The subset sum has to be half the sum of all the elements of the given array so if that sum is not divisible return False
+
+
+M/P
 have a set to store the subset sums we encounter
     however, we cant update the set while iterating over it so we make a copy
+    
+base case:
+    we can ignore a value so the sum of nothing is 0
 
+for each element 
+    go through each element of the set
+        add the sum to the set
+we have created all the subsets and now we need to see if the target exists
+
+E
+O(N * sum(nums)) time complexity where N is the length of the given array, nums. The size of our set is limited to the sum of the elements in the given array.
 """
 
 class Solution:
@@ -16,12 +33,12 @@ class Solution:
         dp = set()
         dp.add(0) # we can choose to ignore a value
         
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums)):
             
             temp = set(dp) # make a copy
             for partition in dp:
                 temp.add(partition + nums[i])
                 
-            dp = temp
+            dp = temp # update the set
         
         return True if target in dp else False
